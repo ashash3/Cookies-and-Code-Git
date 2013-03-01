cccf85a37d0f8546cee84d949308abf9986a7342
BMillman19 <brandon.millman@gmail.com>
2013-03-01 14:04:26 -0500

    Started searchalgos

---
+++ SearchAlgos.java
@@ -0,0 +1,21 @@
+public class SearchAlgos {
+	// Basic DFS algorithm
+
+	// Assuming we have the following Node class definition
+	public Node {
+		ArrayList<Node> children;
+		public boolean visited;
+		
+		public ArrayList<Node> getChildren() {
+			return children;
+		}
+	}
+
+	// Recursive implementation
+	public void dfs_recursive(Node n) {
+	}
+
+	// Stack implementation
+	public void dfs_iterative(Node root) {
+	}
+}
